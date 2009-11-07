0fbeaed08c364c5e2f0df8c7e80e84b835b30dbd
dknowles <dknowles2@gmail.com>
2009-11-07 11:53:51 -0500

    Cleaning up member_loader a bit; adding a unittest.

--- member_loader.py
+++ member_loader.py
@@ -1,38 +1,53 @@
+#!/usr/bin/env python
+
+"""Bulk loader for loading members into datastore."""
+
 import datetime
 import hashlib
 import random
+import string
 
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
+
 import freesidemodels
 
-def randompassword(x):
-  alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890'
-  string=''
-  for x in random.sample(alphabet,10):
-    string+=x
-  return hashlib.sha256(string).digest()
+
+def str_to_bool(s):
+  """Casts a string like "TRUE" or "FALSE" into a bool.
+
+  Args:
+    s: str, boolean string--either "TRUE" or "FALSE"
+  Returns:
+    bool
+  """
+  return s == 'TRUE'
 
 
+def random_password():
+  """Generates a sha256-encoded random ten letter password.
 
-def truefalse(x):
-  if x == 'TRUE':
-    return True
-  else:
-    return False
+  Returns:
+    str, sha256-encoded random ten letter password.
+  """
+  password = ''.join(random.sample(string.lowercase + string.digits, 10))
+  return hashlib.sha256(password).digest()
+
 
 class MemberLoader(bulkloader.Loader):
-  """Parse csv and upload it."""
+  """Parse CSV of members and upload it to datastore."""
+
   def __init__(self):
-    bulkloader.Loader.__init__(self, 'Member',
-                               [('username', str),
-                                ('email', db.Email),
-                                ('active', truefalse),
-                                ('starving', truefalse),
-                                ('joined', 
-                                  lambda x: datetime.datetime.strptime(x, '%m/%d/%Y').date()),
-                                ('rfid', int),
-                                ('password', randompassword),
-                               ])
+    bulkloader.Loader.__init__(
+      self,
+      'Member',
+      [('username', str),
+       ('email', db.Email),
+       ('active', lambda x : x == 'TRUE'),
+       ('starving', truefalse),
+       ('joined', lambda x: datetime.datetime.strptime(x, '%m/%d/%Y').date()),
+       ('rfid', int),
+       ('password', random_password)])
+
 
 loaders = [MemberLoader]
